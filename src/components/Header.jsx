4aa5494d1159f2d799883bf075829f04ce2122fa
benFred <114417668+b-fred84@users.noreply.github.com>
2025-11-29 22:09:30 +0100

    first push

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header({ text = "Feedback UI" }) {
+  const headerStyles = {
+    backgroundColor: "rgba(0,0,0,0.4)",
+    color: "#ff6a95",
+  };
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
