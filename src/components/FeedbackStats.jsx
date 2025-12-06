0b97f48a4ef556574b753b9b44e96ee7f3da1bdf
benFred <114417668+b-fred84@users.noreply.github.com>
2025-12-06 14:09:40 +0100

    add context, alter code to use context.   make update/edit functionality for existing posts

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,15 +1,23 @@
-function FeedbackStats({ feedback }) {
-  let avarage =
-    feedback.reduce((acc, cur) => {
-      return acc + cur.rating;
-    }, 0) / feedback.length;
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-  avarage = avarage.toFixed(1).replace(/[.,]0$/, "");
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
 
+  // let avarage =
+  //   feedback.reduce((acc, cur) => {
+  //     return acc + cur.rating;
+  //   }, 0) / feedback.length;
+
+  const average = feedback.length === 0
+      ? 0
+      : feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length
+
+  
   return (
-    <div className="feedback-stats">
+       <div className='feedback-stats'>
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: {isNaN(avarage) ? 0 : avarage}</h4>
+      <h4>Average Rating: {average.toFixed(1).replace(/[.,]0$/, '')}</h4>
     </div>
   );
 }
